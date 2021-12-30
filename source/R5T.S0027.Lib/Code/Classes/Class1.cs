f7e72519ba573652d25bc26233b0d3a9f3cea3da
davidcoats <david@getsafetycone.com>
2021-12-30 13:20:12 -0800

    Initial commit of source-stub.

---
+++ source/R5T.S0027.Lib/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.S0027.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/R5T.S0027/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0027
+{
+    /// <summary>
+    /// Add project reference selected via IProjectPath extension (and all new dependencies recursively) to specific project, and all solutions in parent directories referencing the specified project.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
