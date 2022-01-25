ce110c126003407e9aded479d3a62534d7c56e46
David Coats <david@getsafetycone.com>
2022-01-24 17:20:19 -0800

    Trivial changes.

--- source/R5T.S0027/Code/Operations/O000_Main.cs
+++ source/R5T.S0027/Code/Operations/O000_Main.cs
@@ -22,10 +22,13 @@ namespace R5T.S0027
         public async Task Run()
         {
             // Inputs.
-            var projectToModifyFilePath = @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.E1000.Private\source\TestProject\TestProject.csproj";
+            var projectToModifyFilePath = @"C:\Code\DEV\Git\GitHub\SafetyCone\R5T.S0026\source\R5T.S0026\R5T.S0026.csproj";
             var projectReferencesToAddIdentityStrings = new[]
             {
-                Instances.ProjectPath.R5T_A0003(), // 130 recursive project reference dependencies!
+                Instances.ProjectPath.R5T_D0077_A002(),
+                Instances.ProjectPath.R5T_D0078_A002(),
+                Instances.ProjectPath.R5T_D0079_A002(),
+                Instances.ProjectPath.R5T_D0083_I001(),
             };
 
             // Run.
